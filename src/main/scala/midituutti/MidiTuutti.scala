90b5d880ecbc7b5fafb5397c451d7d2a46ee10dc
Mikko Suniala <mikko.suniala@iki.fi>
2018-12-06 19:16:48 +0200

    A poc for playing a midi file with Java MIDI system

---
+++ src/main/scala/midituutti/MidiTuutti.scala
@@ -0,0 +1,35 @@
+package midituutti
+
+import java.io.File
+
+import javax.sound.midi._
+
+object MidiTuutti extends App {
+  val filePath = if (args.length == 1) args(0) else throw new IllegalArgumentException("must give path to midi file")
+
+  val synthesizer = MidiSystem.getSynthesizer
+  synthesizer.open()
+  println(s"using synthesizer $synthesizer")
+
+  val receiver = synthesizer.getReceiver
+  println(s"using receiver $receiver")
+
+  println(s"opening midi file $filePath")
+  val seq = MidiSystem.getSequence(new File(filePath))
+
+  val currentTempoInBeatsPerMinute = 120
+  val ticksPerSecond = seq.getResolution * (currentTempoInBeatsPerMinute / 60.0)
+  val tickSize = 1.0 / ticksPerSecond
+
+  val track: Track = seq.getTracks.apply(0)
+
+  for (eventI <- 0 until track.size) {
+    val event = track.get(eventI)
+    // Send all messages with timestamps as fast as possible. The receiver will take care of playing each message
+    // at the given time.
+    receiver.send(event.getMessage, (event.getTick * tickSize * 1000 * 1000).toLong)
+  }
+
+  // Sleep for a while so as to give the synthesizer a change to play something.
+  Thread.sleep(5 * 1000)
+}
