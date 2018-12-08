0e56770a0bb7069d3a6e00ad6f14ed188a166c46
Mikko Suniala <mikko.suniala@iki.fi>
2018-12-08 22:03:39 +0200

    Separate reader and player threads

--- src/main/scala/midituutti/MidiTuutti.scala
+++ src/main/scala/midituutti/MidiTuutti.scala
@@ -1,8 +1,11 @@
 package midituutti
 
-import midituutti.midi.{OutputTimestamp, Tempo, Tick}
+import java.util.concurrent.SynchronousQueue
+
+import midituutti.midi.{MidiEvent, OutputTimestamp, Tempo, Tick}
 
 import scala.io.StdIn
+
 
 object MidiTuutti extends App {
   val filePath = if (args.length == 1) args(0) else throw new IllegalArgumentException("must give path to midi file")
@@ -11,19 +14,58 @@ object MidiTuutti extends App {
   val midiFile = midi.openFile(filePath)
   val tempo = Tempo(120.0)
 
-  var prevTicks = new Tick(0)
-  for (event <- midiFile.track) {
-    val ticksDelta = event.ticks - prevTicks
-    val timestampDelta = OutputTimestamp.ofTickAndTempo(ticksDelta, midiFile.resolution, tempo)
+  @volatile var playing = true
+  val queue = new SynchronousQueue[MidiEvent]
 
-    if (timestampDelta.nonNil) {
-      Thread.sleep(timestampDelta.millisPart, timestampDelta.nanosPart)
+  val reader: Thread = new Thread {
+    override def run(): Unit = {
+      while (playing) {
+        try {
+          for (event <- midiFile.track) {
+            queue.put(event)
+          }
+        } catch {
+          case _: InterruptedException =>
+        }
+      }
+
+      println("reader done")
     }
-
-    synthesizerPort.send(event.message)
-
-    prevTicks = event.ticks
   }
 
+  val player: Thread = new Thread {
+    override def run(): Unit = {
+      var prevTicks: Option[Tick] = None
+
+      try {
+        while (playing) {
+          val event = queue.take()
+
+          val ticksDelta = event.ticks - prevTicks.getOrElse(event.ticks)
+          val timestampDelta = OutputTimestamp.ofTickAndTempo(ticksDelta, midiFile.resolution, tempo)
+
+          if (timestampDelta.nonNil) {
+            Thread.sleep(timestampDelta.millisPart, timestampDelta.nanosPart)
+          }
+
+          synthesizerPort.send(event.message)
+
+          prevTicks = Some(event.ticks)
+        }
+      } catch {
+        case _: InterruptedException =>
+      }
+
+      println("player done")
+    }
+  }
+
+  player.start()
+  reader.start()
+
   StdIn.readLine("Press enter to quit.")
+
+  playing = false
+  player.interrupt()
+  reader.interrupt()
 }
