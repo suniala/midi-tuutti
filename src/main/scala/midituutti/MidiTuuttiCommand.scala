126aa970e8476ae687e14a3f44ce083ea835339d
Mikko Suniala <mikko.suniala@iki.fi>
2018-12-14 22:57:38 +0200

    Refactor: rename app as MidiTuuttiCommand.scala

--- src/main/scala/midituutti/MidiTuuttiCommand.scala
+++ src/main/scala/midituutti/MidiTuuttiCommand.scala
@@ -4,7 +4,7 @@ import midituutti.engine.createEngine
 
 import scala.io.StdIn
 
-object MidiTuutti extends App {
+object MidiTuuttiCommand extends App {
   val filePath = if (args.length >= 1) args(0) else throw new IllegalArgumentException("must give path to midi file")
   val startMeasure = if (args.length >= 2) Some(args(1).toInt) else None
   val endMeasure = if (args.length >= 3) Some(args(2).toInt) else None
