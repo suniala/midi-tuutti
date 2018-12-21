624dbf24bb5ae780c0853b0e82c0cc5901cbc670
Mikko Suniala <mikko.suniala@iki.fi>
2018-12-21 23:28:35 +0200

    Replace stop button with a play toggle, add and refactor shortcuts

--- src/main/scala/midituutti/MidiTuutti.scala
+++ src/main/scala/midituutti/MidiTuutti.scala
@@ -1,12 +1,14 @@
 package midituutti
 
+import javafx.event.EventHandler
+import javafx.scene.input.KeyEvent
 import midituutti.engine.createEngine
 import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.geometry.Insets
 import scalafx.scene.Scene
-import scalafx.scene.control.{Button, ToggleButton}
+import scalafx.scene.control.ToggleButton
 import scalafx.scene.input.KeyCode
 import scalafx.scene.layout.HBox
 
@@ -16,42 +18,41 @@ object MidiTuutti extends JFXApp {
   private val engine = createEngine(filePath, None, None)
   private val drumChannel = 10
 
+  private val playButton: ToggleButton = new ToggleButton {
+    text = "Play"
+    selected = engine.isPlaying
+    onAction = handle {
+      if (engine.isPlaying) engine.stop()
+      else engine.play()
+    }
+    focusTraversable = false
+  }
+
+  private val muteButton: ToggleButton = new ToggleButton {
+    text = "Mute drums"
+    selected = engine.isMuted(drumChannel)
+    onAction = handle {
+      if (engine.isMuted(drumChannel)) engine.unMute(drumChannel)
+      else engine.mute(drumChannel)
+    }
+    focusTraversable = false
+  }
+
+  private val keyHandler: EventHandler[_ >: KeyEvent] = k => k.code match {
+    case KeyCode.Space => playButton.fire()
+    case KeyCode.M => muteButton.fire()
+    case _ => // ignore
+  }
+
   stage = new PrimaryStage {
     title = "MidiTuutti"
     scene = new Scene {
-      onKeyPressed = k => k.code match {
-        case KeyCode.Space if !engine.isPlaying =>
-          engine.play()
-        case KeyCode.Space if engine.isPlaying =>
-          engine.stop()
-        case _ => // ignore
-      }
+      onKeyPressed = keyHandler
       content = new HBox {
         padding = Insets(20)
         children = Seq(
-          new Button {
-            text = "Play"
-            onAction = handle {
-              engine.play()
-            }
-            focusTraversable = false
-          },
-          new Button {
-            text = "Stop"
-            onAction = handle {
-              engine.stop()
-            }
-            focusTraversable = false
-          },
-          new ToggleButton {
-            text = "Mute drums"
-            selected = engine.isMuted(drumChannel)
-            onAction = handle {
-              if (engine.isMuted(drumChannel)) engine.unMute(drumChannel)
-              else engine.mute(drumChannel)
-            }
-            focusTraversable = false
-          }
+          playButton,
+          muteButton
         )
       }
     }
