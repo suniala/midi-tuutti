4f2040033b439fb19f8cce2405a668590505ae79
Mikko Suniala <mikko.suniala@iki.fi>
2018-12-23 20:21:16 +0200

    Inject a click track that can't be turned off!

--- src/main/scala/midituutti/engine/TrackStructure.scala
+++ src/main/scala/midituutti/engine/TrackStructure.scala
@@ -1,6 +1,6 @@
 package midituutti.engine
 
-import midituutti.midi.MessageDecoder.{Accessors, TimeSignature}
+import midituutti.midi.MessageDecoder.{Accessors, Note, OnOff, TimeSignature}
 import midituutti.midi._
 
 import scala.annotation.tailrec
@@ -11,15 +11,33 @@ class Measure(val timeSignature: TimeSignature, val events: Seq[MidiEvent])
 class TrackStructure(val measures: Seq[Measure])
 
 object TrackStructure {
+
   def of(midiFile: MidiFile): TrackStructure = {
     val track = midiFile.events
     val timeSignatureMessage = track.find(_.message.metaType.contains(MetaType.TimeSignature)).get.message
     val measures = new Parser(midiFile.ticksPerBeat).parse(timeSignatureMessage, track)
-    new TrackStructure(measures)
+    val clickMeasures = injectClick(measures, midiFile.ticksPerBeat)
+    new TrackStructure(clickMeasures)
   }
 
   def measureTicks(ticksPerBeat: Int, timeSignature: TimeSignature): Tick =
     new Tick(ticksPerBeat * timeSignature.numerator / (timeSignature.denominator / 4))
+
+  def beatTicks(beat: Int, ticksPerBeat: Int, timeSignature: TimeSignature): Tick =
+    new Tick(ticksPerBeat * (beat - 1) / (timeSignature.denominator / 4))
+
+  private def injectClick(measures: Seq[Measure], ticksPerBeat: Int): Seq[Measure] = {
+    def measureClick(measure: Measure): Measure = {
+      val clickEventTicks = (1 to measure.timeSignature.numerator)
+        .map(measure.events.head.ticks + beatTicks(_, ticksPerBeat, measure.timeSignature))
+      val clickEvents = clickEventTicks
+        .zipWithIndex
+        .map({ case (t, _) => new MidiEvent(t, NoteMessage(Note(OnOff.On, 10, 42, 100))) })
+      new Measure(measure.timeSignature, (clickEvents ++ measure.events).sortWith({ case (a, b) => a.ticks < b.ticks }))
+    }
+
+    measures.map(measureClick)
+  }
 
   private class Parser(ticksPerBeat: Int) {
     def parse(ts: MidiMessage, track: Seq[MidiEvent]): Seq[Measure] = {
