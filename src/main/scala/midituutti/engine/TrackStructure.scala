suniala/midi-tuutti

commit 1db5a565b7437ee278c317b5daec1a5b810b2c75

Refactor: Make time signature a value of Measure instead of an event
Could not figure out why would we need to have each bar's time
signature as an event as we are not sending them anywhere.

src/main/scala/midituutti/engine/TrackStructure.scala
@@ -6,7 +6,7 @@ import midituutti.midi._
 import scala.annotation.tailrec
 import scala.collection.immutable
 
-class Measure(val events: Seq[MidiEvent])
+class Measure(val timeSignature: TimeSignature, val events: Seq[MidiEvent])
 
 class TrackStructure(val measures: Seq[Measure])
 
@@ -31,13 +31,6 @@ object TrackStructure {
       delta < measureTicks(ticksPerBeat, timeSignatureMessage.get(Accessors.timeSignatureAccessor))
     }
 
-    private def timeSignatureEvent(eventTimeSignature: MidiMessage, prevMeasureStart: MidiEvent,
-                                   prevTimeSignature: MidiMessage) = {
-      new MidiEvent(
-        prevMeasureStart.ticks + measureTicks(ticksPerBeat, prevTimeSignature.get(Accessors.timeSignatureAccessor)),
-        eventTimeSignature)
-    }
-
     @tailrec
     private def parseRec(acc: immutable.List[Measure],
                          currTimeSignature: MidiMessage,
@@ -45,7 +38,7 @@ object TrackStructure {
                          rem: Seq[MidiEvent]): Seq[Measure] = {
       // TODO: get rid of this cons + reverse silliness
       if (rem.isEmpty) {
-        (new Measure(measure.reverse) :: acc).reverse
+        (new Measure(currTimeSignature.get(Accessors.timeSignatureAccessor), measure.reverse) :: acc).reverse
       } else {
         val event = rem.head
         val measureStart = measure.last
@@ -59,8 +52,9 @@ object TrackStructure {
         if (withinMeasure(event, currTimeSignature, measureStart)) {
           parseRec(acc, nextTimeSignature, event :: measure, rem.tail)
         } else {
-          parseRec(new Measure(measure.reverse) :: acc, nextTimeSignature,
-            event :: timeSignatureEvent(nextTimeSignature, measureStart, currTimeSignature) :: Nil,
+          parseRec(new Measure(currTimeSignature.get(Accessors.timeSignatureAccessor), measure.reverse) :: acc,
+            nextTimeSignature,
+            event :: Nil,
             rem.tail)
         }
       }

src/test/scala/midituutti/engine/TrackStructureSpec.scala
@@ -3,39 +3,18 @@ package midituutti.engine
 import java.io.InputStream
 
 import midituutti.midi
-import midituutti.midi.MessageDecoder.{Accessors, TimeSignature}
-import midituutti.midi.{MetaType, MidiEvent, Tick}
-import org.scalatest.matchers.{MatchResult, Matcher}
+import midituutti.midi.MessageDecoder.TimeSignature
+import midituutti.midi.Tick
 import org.scalatest.{FunSpec, Matchers}
 
-trait MyMatchers {
-
-  class EventSetsTimeSignatureToMatcher(expectedTimeSignature: TimeSignature) extends Matcher[MidiEvent] {
-    def apply(left: MidiEvent): MatchResult = {
-      val message = left.message
-      MatchResult(
-        message.metaType.contains(MetaType.TimeSignature)
-          && message.get(Accessors.timeSignatureAccessor) == expectedTimeSignature,
-        s"$left does not set time signature to $expectedTimeSignature",
-        s"$left does set time signature to $expectedTimeSignature"
-      )
-    }
-  }
-
-  def setTimeSignatureTo(expectedExtension: TimeSignature) = new EventSetsTimeSignatureToMatcher(expectedExtension)
-}
-
-
-//noinspection ZeroIndexToHead
-class TrackStructureSpec extends FunSpec with Matchers with MyMatchers {
+class TrackStructureSpec extends FunSpec with Matchers {
 
   case class Expectation(timeSignature: TimeSignature, firstTick: Tick, eventCount: Int)
 
   describe("TrackStructure parsing") {
 
     /**
-      * NOTE: This is a very brittle test. Event counts and head event ticks have been checked with a Midi debugger.
-      * Event count also depends on our implementation as events may be injected.
+      * NOTE: This is a somewhat brittle test. Event counts and head event ticks have been checked with a Midi debugger.
       */
     it("should parse varying time signatures correctly") {
       val midiFile = midi.openFile(testFile("measures-44-34-58.mid"))
@@ -43,12 +22,12 @@ class TrackStructureSpec extends FunSpec with Matchers with MyMatchers {
 
       val expectations = List(
         Expectation(TimeSignature(4, 4), Tick(0), 19),
-        Expectation(TimeSignature(4, 4), Tick(1920), 9),
-        Expectation(TimeSignature(3, 4), Tick(3840), 8),
-        Expectation(TimeSignature(3, 4), Tick(5280), 7),
-        Expectation(TimeSignature(5, 8), Tick(6720), 12),
-        Expectation(TimeSignature(5, 8), Tick(7920), 11),
-        Expectation(TimeSignature(4, 4), Tick(9120), 11),
+        Expectation(TimeSignature(4, 4), Tick(1920), 8),
+        Expectation(TimeSignature(3, 4), Tick(3840), 7),
+        Expectation(TimeSignature(3, 4), Tick(5280), 6),
+        Expectation(TimeSignature(5, 8), Tick(6720), 11),
+        Expectation(TimeSignature(5, 8), Tick(7920), 10),
+        Expectation(TimeSignature(4, 4), Tick(9120), 10),
       )
 
       track.measures should have length expectations.length
@@ -56,8 +35,8 @@ class TrackStructureSpec extends FunSpec with Matchers with MyMatchers {
       track.measures.zip(expectations.indices.zip(expectations)).foreach {
         case (measure, (index, expected)) =>
           withClue(s"At measure $index, ") {
-            withClue("first event ") {
-              measure.events.head should setTimeSignatureTo(expected.timeSignature)
+            withClue("measure time signature ") {
+              measure.timeSignature shouldBe expected.timeSignature
             }
             withClue("first event ") {
               measure.events.head.ticks shouldBe expected.firstTick
@@ -69,8 +48,4 @@ class TrackStructureSpec extends FunSpec with Matchers with MyMatchers {
   }
 
   private def testFile(file: String): InputStream = getClass.getResourceAsStream(file)
-
-  private def measure(track: TrackStructure, index: Int)(a: Measure => Unit): Unit = {
-    a(track.measures(index))
-  }
 }

commit 71ee552d8e9b9c2cabf27b4287db040d6681c064

Bugfix: Fix measure calculation that got broken in 1db5a56
In said refactoring, I forgot that the first event of a measure is not
necessarily at the measure start. Make start tick a property of Measure
and calculate it when parsing Measures.

Note that this also fixes click injection in cases where there isn't
any event at measure start. Also give the test a track without the
click so as to make the test more focused (a bit clumsy but works for
now).

src/main/scala/midituutti/engine/TrackStructure.scala
@@ -6,72 +6,82 @@ import midituutti.midi._
 import scala.annotation.tailrec
 import scala.collection.immutable
 
-class Measure(val timeSignature: TimeSignature, val events: Seq[MidiEvent])
+class Measure(val start: Tick, val timeSignature: TimeSignature, val events: Seq[MidiEvent])
 
 class TrackStructure(val measures: Seq[Measure])
 
 object TrackStructure {
 
-  def of(midiFile: MidiFile): TrackStructure = {
+  def of(midiFile: MidiFile): TrackStructure = new TrackStructure(measures(midiFile))
+
+  def withClick(midiFile: MidiFile): TrackStructure =
+    new TrackStructure(injectClick(measures(midiFile), midiFile.ticksPerBeat))
+
+  private def measures(midiFile: MidiFile) = {
     val track = midiFile.events
-    val timeSignatureMessage = track.find(_.message.metaType.contains(MetaType.TimeSignature)).get.message
-    val measures = new Parser(midiFile.ticksPerBeat).parse(timeSignatureMessage, track)
-    val clickMeasures = injectClick(measures, midiFile.ticksPerBeat)
-    new TrackStructure(clickMeasures)
+    val timeSignatureEvent = track.find(_.message.metaType.contains(MetaType.TimeSignature)).get
+    new Parser(midiFile.ticksPerBeat).parse(timeSignatureEvent, track)
   }
 
-  def measureTicks(ticksPerBeat: Int, timeSignature: TimeSignature): Tick =
+  private def measureTicks(ticksPerBeat: Int, timeSignature: TimeSignature): Tick =
     new Tick(ticksPerBeat * timeSignature.numerator / (timeSignature.denominator / 4))
 
-  def beatTicks(beat: Int, ticksPerBeat: Int, timeSignature: TimeSignature): Tick =
+  private def beatTicks(beat: Int, ticksPerBeat: Int, timeSignature: TimeSignature): Tick =
     new Tick(ticksPerBeat * (beat - 1) / (timeSignature.denominator / 4))
 
   private def injectClick(measures: Seq[Measure], ticksPerBeat: Int): Seq[Measure] = {
     def measureClick(measure: Measure): Measure = {
       val clickEventTicks = (1 to measure.timeSignature.numerator)
-        .map(measure.events.head.ticks + beatTicks(_, ticksPerBeat, measure.timeSignature))
+        .map(measure.start + beatTicks(_, ticksPerBeat, measure.timeSignature))
       val clickEvents = clickEventTicks
         .zipWithIndex
         .map({ case (t, _) => new MidiEvent(t, NoteMessage(Note(OnOff.On, 10, 42, 100))) })
-      new Measure(measure.timeSignature, (clickEvents ++ measure.events).sortWith({ case (a, b) => a.ticks < b.ticks }))
+      new Measure(measure.start,
+        measure.timeSignature,
+        (clickEvents ++ measure.events).sortWith({ case (a, b) => a.ticks < b.ticks }))
     }
 
     measures.map(measureClick)
   }
 
   private class Parser(ticksPerBeat: Int) {
-    def parse(ts: MidiMessage, track: Seq[MidiEvent]): Seq[Measure] = {
-      parseRec(Nil, ts, new MidiEvent(new Tick(0), ts) :: Nil, track)
+    def parse(ts: MidiEvent, track: Seq[MidiEvent]): Seq[Measure] = {
+      parseRec(Nil, ts.message.get(Accessors.timeSignatureAccessor), ts.ticks, Nil, track)
     }
 
-    private def withinMeasure(event: MidiEvent, timeSignatureMessage: MidiMessage, measureStart: MidiEvent): Boolean = {
-      val delta = event.ticks - measureStart.ticks
-      delta < measureTicks(ticksPerBeat, timeSignatureMessage.get(Accessors.timeSignatureAccessor))
+    private def withinMeasure(event: MidiEvent, timeSignature: TimeSignature, measureStart: Tick): Boolean = {
+      val delta = event.ticks - measureStart
+      delta < measureTicks(ticksPerBeat, timeSignature)
     }
+
+    private def nextMeasureStart(start: Tick, timeSignature: TimeSignature): Tick =
+      start + measureTicks(ticksPerBeat, timeSignature)
 
     @tailrec
     private def parseRec(acc: immutable.List[Measure],
-                         currTimeSignature: MidiMessage,
+                         currTimeSignature: TimeSignature,
+                         measureStart: Tick,
                          measure: immutable.List[MidiEvent],
                          rem: Seq[MidiEvent]): Seq[Measure] = {
       // TODO: get rid of this cons + reverse silliness
       if (rem.isEmpty) {
-        (new Measure(currTimeSignature.get(Accessors.timeSignatureAccessor), measure.reverse) :: acc).reverse
+        (new Measure(measureStart, currTimeSignature, measure.reverse) :: acc).reverse
       } else {
         val event = rem.head
-        val measureStart = measure.last
 
         val nextTimeSignature =
           if (event.message.metaType.contains(MetaType.TimeSignature)) {
-            event.message
+            event.message.get(Accessors.timeSignatureAccessor)
           }
           else currTimeSignature
 
         if (withinMeasure(event, currTimeSignature, measureStart)) {
-          parseRec(acc, nextTimeSignature, event :: measure, rem.tail)
+          parseRec(acc, nextTimeSignature, measureStart, event :: measure, rem.tail)
         } else {
-          parseRec(new Measure(currTimeSignature.get(Accessors.timeSignatureAccessor), measure.reverse) :: acc,
+          parseRec(
+            new Measure(measureStart, currTimeSignature, measure.reverse) :: acc,
             nextTimeSignature,
+            nextMeasureStart(measureStart, currTimeSignature),
             event :: Nil,
             rem.tail)
         }

src/main/scala/midituutti/engine/package.scala
@@ -81,7 +81,7 @@ package object engine {
   def createEngine(filePath: String, initialFrom: Option[Int], initialTo: Option[Int]): Engine = {
     val synthesizerPort = midi.createDefaultSynthesizerPort
     val midiFile = midi.openFile(filePath)
-    val track = TrackStructure.of(midiFile)
+    val track = TrackStructure.withClick(midiFile)
 
     val queue = new SynchronousQueue[MidiEvent]
 

src/test/scala/midituutti/engine/TrackStructureSpec.scala
@@ -21,7 +21,7 @@ class TrackStructureSpec extends FunSpec with Matchers {
       val track = TrackStructure.of(midiFile)
 
       val expectations = List(
-        Expectation(TimeSignature(4, 4), Tick(0), 19),
+        Expectation(TimeSignature(4, 4), Tick(0), 18),
         Expectation(TimeSignature(4, 4), Tick(1920), 8),
         Expectation(TimeSignature(3, 4), Tick(3840), 7),
         Expectation(TimeSignature(3, 4), Tick(5280), 6),